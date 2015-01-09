d0e28853c02bd8eabdfc51269e502c5921d6c654
yshiventuretech <yshi@venturetechasia.net>
2015-01-09 03:41:25 -0600

    print hellowrld

---
+++ HelloWorld/src/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package demo;
+
+/**
+ * FIXME : Document this class.
+ *
+ * @author Yumei Shi (yshi@venturetechasia.net)
+ * @since 1/9/15 2:38 AM
+ */
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
